c0ed8ef2e20939a82ba63634491e4ce23353f2bf
MANISHA BOSE <manishabose.2001@gmail.com>
2025-03-31 22:41:05 +0530

    Fix hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable || ""} ${
+          geistMono.variable || ""
+        } antialiased`}
       >
         <AppBar />
         {children}
