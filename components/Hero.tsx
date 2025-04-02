478a1d1ff9d5c361ad0408dbc61865380f07c621
MANISHA BOSE <manishabose.2001@gmail.com>
2025-04-02 21:03:31 +0530

    Add content in the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,11 @@
 export default function Hero() {
-  return <div>Shree Computers</div>;
+  return (
+    <div>
+      <div>Shree Computers</div>
+      <div>Master the digital world:</div>
+      <div>From Basics to Beyond</div>
+      <div>Explore Courses</div>
+      <div>Contact</div>
+    </div>
+  );
 }
