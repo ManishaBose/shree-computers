0df5576d8c3eaac0f7ea6b94456c65b8dc4c9a55
MANISHA BOSE <manishabose.2001@gmail.com>
2025-04-03 22:19:52 +0530

    Add Courses

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -3,9 +3,37 @@ export default function Courses() {
     <div>
       <div>Search Bar</div>
       <div>Filter by Qualification, Duration, Level, Fees</div>
-      <div>Course1</div>
-      <div>Course2</div>
-      <div>So on</div>
+      <div>'O' Level</div>
+      <div>Web Designing and Publishing</div>
+      <div>Certificate Course in Computer Concepts</div>
+      <div>CCC Plus</div>
+      <div>Web Developer</div>
+      <div>Data Entry and Office Assistant</div>
+      <div>
+        Certified Computer Application, Accounting, and Publishing Assistant
+      </div>
+      <div>Financial Accounting</div>
+      <div>Diploma in Financial Accounting</div>
+      <div>Financial Accounting Course & Advanced Excel</div>
+      <div>‘A’ LEVEL</div>
+      <div>Advanced Diploma in Computer Application</div>
+      <div>Certificate Course in PC Hardware & Networking</div>
+      <div>Desktop Publishing</div>
+      <div>Diploma in Computer Application</div>
+      <div>Post Graduate Diploma in Computer Application</div>
+      <div>Windows</div>
+      <div>Linux</div>
+      <div>C Programming</div>
+      <div>C++ Programming</div>
+      <div>Basic Language</div>
+      <div>Visual Basic</div>
+      <div>MS Office</div>
+      <div>Online Internet Training</div>
+      <div>MS Access</div>
+      <div>Java Programming</div>
+      <div>HTML</div>
+      <div>Data Structures using C</div>
+      <div>Oracle</div>
     </div>
   );
 }
