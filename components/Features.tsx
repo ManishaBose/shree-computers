284e660f465f8a2d3c79a8a8fd87be92366dcc34
MANISHA BOSE <manishabose.2001@gmail.com>
2025-04-27 21:21:07 +0530

    Features format

--- components/Features.tsx
+++ components/Features.tsx
@@ -3,20 +3,31 @@ export default function () {
     <div>
       <div>FEATURES</div>
       <div>
-        1. Only Accredited Centre in Katras Township Proudly authorised (ACCR
-        No. O1476) to conduct NIELIT-certified ‘O’ Level, CCC & BCC courses —
-        your trusted gateway to government-recognised computer education. 2.
-        Awarded for Quality Education Certified by DOEACC Society for delivering
-        ‘Quality Computer Education’ — with a stellar record in NIELIT exam
-        success. 3. Expert Faculty & Modern Infrastructure Learn from a
-        dedicated team of qualified instructors in a fully LAN-connected lab
-        with the latest hardware, projectors, printers, scanners, and high-speed
-        internet. 4. Comprehensive Facilities & Student Comfort Benefit from a
-        rich library, clean restrooms, separate refreshment areas, and cool,
-        fresh drinking water — all tailored for a comfortable learning
-        experience. 5. Beyond Courses: Growth & Placement Get personalised
-        attention, expert-led personality development sessions, and active
-        placement assistance to help you thrive beyond the classroom.
+        <div>
+          1. Only Accredited Centre in Katras Township Proudly authorised (ACCR
+          No. O1476) to conduct NIELIT-certified ‘O’ Level, CCC & BCC courses —
+          your trusted gateway to government-recognised computer education.
+        </div>
+        <div>
+          2. Awarded for Quality Education Certified by DOEACC Society for
+          delivering ‘Quality Computer Education’ — with a stellar record in
+          NIELIT exam success.
+        </div>
+        <div>
+          3. Expert Faculty & Modern Infrastructure Learn from a dedicated team
+          of qualified instructors in a fully LAN-connected lab with the latest
+          hardware, projectors, printers, scanners, and high-speed internet.
+        </div>
+        <div>
+          4. Comprehensive Facilities & Student Comfort Benefit from a rich
+          library, clean restrooms, separate refreshment areas, and cool, fresh
+          drinking water — all tailored for a comfortable learning experience.
+        </div>
+        <div>
+          5. Beyond Courses: Growth & Placement Get personalised attention,
+          expert-led personality development sessions, and active placement
+          assistance to help you thrive beyond the classroom.
+        </div>
       </div>
     </div>
   );
